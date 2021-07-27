e76405f8e826c098ac5bac4d0881a65a17c40fdc
SabriM2401 <sabrina.jmeli@gmail.com>
2021-07-27 10:53:03 -0300

    Se crean los Vuelo/Aeropuerto Repo, y un Model de Response, GenericResponse.

---
+++ src/main/java/ar/com/ada/api/aladas/repos/AeropuertoRepository.java
@@ -0,0 +1,12 @@
+package ar.com.ada.api.aladas.repos;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import ar.com.ada.api.aladas.entities.Aeropuerto;
+
+@Repository
+public interface AeropuertoRepository extends JpaRepository<Aeropuerto, Integer> {
+    
+    Aeropuerto findByCodigoIATA(String codigoIATA); 
+}
